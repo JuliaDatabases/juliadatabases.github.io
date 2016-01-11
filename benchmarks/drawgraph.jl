5cd3c1e0e8da1273e3926e3b3bb7a2142c2e7cfc
nkottary <kottarynishanth54@gmail.com>
2016-01-11 10:26:33 +0530

    Added performance graphs

---
+++ benchmarks/drawgraph.jl
@@ -0,0 +1,11 @@
+using Gadfly
+using DataFrames
+
+bench_plot(x, y, kind, sz) =
+    draw(PNG("$kind.png", sz...), plot(x=x, y=y, Geom.bar, Guide.xlabel("Database Drivers"), Guide.ylabel("Time in seconds"), Guide.title(uppercase(kind)), color=x))
+
+benchmarks = readtable("benchmarks.csv")
+x = benchmarks[:DataBase_Drivers]
+bench_plot(x, benchmarks[:Insert_time], "insert", (8inch, 6inch))
+bench_plot(x, benchmarks[:Update_time], "update", (8inch, 6inch))
+bench_plot(x, benchmarks[:Retrieval_time], "select", (8inch, 6inch))
